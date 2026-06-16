46a7749f1a54e80396cb5a6af65d791225a0f553
HasanthaLankadeva <41467475+HasanthaLankadeva@users.noreply.github.com>
2026-06-17 02:56:30 +0300

    latest upfates

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -61,12 +61,6 @@
 	<link rel="stylesheet" href="<?= PUBLIC_URL ?>/assets/css/project/theme-media-1024.css" media="(min-width: 1024px)">
 	<link rel="stylesheet" href="<?= PUBLIC_URL ?>/assets/css/project/theme-media-1366.css" media="(min-width: 1366px)">
 
-	<!-- CMS Defaults JS -->
-	<script src="<?= PUBLIC_URL ?>/assets/js/jquery-3.3.1.min.js"></script>
-	<script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-	<script src="<?= PUBLIC_URL ?>/assets/js/S360-base-v-2.js"></script>
-	<script src="<?= PUBLIC_URL ?>/assets/js/auto-load-libs.js"></script>
-
 	<link rel="stylesheet" href="<?= PUBLIC_URL ?>/assets/js/libs/aos/aos.css" media="(min-width: 1366px)">
 </head>
 <body class="<?= $template . ' ' . $pageClass; ?>">
